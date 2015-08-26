6a0b94e06be02e4aad98bf18ffa10f13fd49007c
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-08-27 07:00:32 +1000

    Hives use entity objects

--- examples/garrows/game/game.js
+++ examples/garrows/game/game.js
@@ -15,10 +15,19 @@ function Game(mapWidth) {
 
 Game.prototype = {
   serverUpdated: function(data) {
+    function updateEntities(entities, serialized) {
+      for (var i = 0; i < serialized.length; i++) {
+        if (!entities[i]) {
+          entities[i] = new Entity(game, 0, 0);
+        }
+        entities[i].deserialize(serialized[i]);
+      }
+    }
     this.counter = data.counter ? data.counter : this.counter;
     this.players = data.players ? data.players : this.players;
     this.food = data.food ? data.food : this.food;
-    this.hives = data.hives ? data.hives : this.hives;
+    // this.hives = data.hives ? data.hives : this.hives;
+    updateEntities(this.hives, data.hives)
     this.creeps = data.creeps ? data.creeps : this.creeps;
   },
   update: function(io) {
@@ -41,25 +50,26 @@ Game.prototype = {
         y: r(w),
         health: 100
       });
-      var hive = {
-        x: r(w),
-        y: r(w),
-        health: 100,
-        creeps: [],
-        update: function(ts) {
-          log('creepCount', hive.creepCount);
-          if (hive.creeps.length < 1) {
-            var creep = {
-              x: r(w),
-              y: r(w),
-              health: 100,
-              update: function(ts) {}
-            }
-            hive.creeps.push(creep);
-            this.creeps.push(creep);
-          }
-        }
-      };
+      // var hive = {
+      //   x: r(w),
+      //   y: r(w),
+      //   health: 100,
+      //   creeps: [],
+      //   update: function(ts) {
+      //     log('creepCount', hive.creepCount);
+      //     if (hive.creeps.length < 1) {
+      //       var creep = {
+      //         x: r(w),
+      //         y: r(w),
+      //         health: 100,
+      //         update: function(ts) {}
+      //       }
+      //       hive.creeps.push(creep);
+      //       this.creeps.push(creep);
+      //     }
+      //   }
+      // };
+      var hive = new Entity(this, r(w), r(w));
       this.hives.push(hive);
     }
   },
@@ -134,18 +144,18 @@ Game.prototype = {
       var p = this.players[i],
         x = p.x * w - (this.cam.x * w),
         y = p.y * w - (this.cam.y * w);
-      log(x, y);
       c.fillRect(x, y, w, w);
       c.strokeRect(x - 1, y - 1, w + 2, w + 2);
     }
-    //Draw players
+    //Draw hives
     c.fillStyle = c.strokeStyle = '#e00';
     for (var i = 0; i < this.hives.length; i++) {
-      var p = this.hives[i],
-        x = p.x * w - (this.cam.x * w),
-        y = p.y * w - (this.cam.y * w);
-      c.fillRect(x, y, w, w);
-      c.strokeRect(x - 1, y - 1, w + 2, w + 2);
+      this.hives[i].draw(ts);
+      // var p = this.hives[i],
+      //   x = p.x * w - (this.cam.x * w),
+      //   y = p.y * w - (this.cam.y * w);
+      // c.fillRect(x, y, w, w);
+      // c.strokeRect(x - 1, y - 1, w + 2, w + 2);
     }
   },
   drawLoop: function(dt) {
@@ -164,3 +174,49 @@ function r(max) {
   var x = Math.sin(s++) * 10000;
   return (x - Math.floor(x)) * max;
 }
+
+
+
+
+
+
+
+function Entity(game, x, y) {
+  console.log('new entity');
+  var t = this;
+  t.color = '#e00';
+  t.game = game;
+  t.x = x;
+  t.y = y;
+}
+
+Entity.prototype = {
+  deserialize: function(data) {
+    var t = this;
+    t.x = data.x;
+    t.y = data.y;
+    t.color = data.color;
+  },
+  draw: function(ts) {
+    var t = this;
+    var w = canvas.width / (lCan.width * t.game.cam.z);
+    //Draw players
+    c.strokeWidth = 1;
+    c.fillStyle = c.strokeStyle = t.color;
+
+    var x = t.x * w - (t.game.cam.x * w),
+      y = t.y * w - (t.game.cam.y * w);
+    c.fillRect(x, y, w, w);
+    c.strokeRect(x - 1, y - 1, w + 2, w + 2);
+
+  },
+  toJSON: function() {
+    var t = this;
+    return {
+      x: t.x,
+      y: t.y,
+      color: t.color
+    }
+  }
+
+};
